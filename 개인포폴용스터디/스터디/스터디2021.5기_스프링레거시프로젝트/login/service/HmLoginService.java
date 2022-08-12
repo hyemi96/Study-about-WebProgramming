9016b26427081a8d50f92d34f85fba2b834142cc
송혜미 <74815320+hyemi96@users.noreply.github.com>
2022-08-12 22:36:11 +0900

    Delete 개인포폴용스터디/스터디/스터디2021.5기_스프링레거시프로젝트 directory

--- 개인포폴용스터디/스터디/스터디2021.5기_스프링레거시프로젝트/login/service/HmLoginService.java
+++
@@ -1,9 +0,0 @@
-package co.kr.wdt.hm.login.service;
-
-import co.kr.wdt.hm.login.vo.HmLoginVo;
-
-public interface HmLoginService {
-
-	public HmLoginVo selectUserLoginCheck(HmLoginVo hmLoginVo);
-
-}
