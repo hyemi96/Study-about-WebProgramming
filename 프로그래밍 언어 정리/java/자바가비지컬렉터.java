2999cd37ed94c5cac36f099e0721bf9602a0ddec
송혜미 <74815320+hyemi96@users.noreply.github.com>
2022-12-21 10:55:54 +0900

    Update and rename 자바가비지컬렉터.md to 자바가비지컬렉터.java

---
+++ 프로그래밍 언어 정리/java/자바가비지컬렉터.java
@@ -0,0 +1,30 @@
+
+
+/* 
+블로그 예시
+*/
+ Person person = new Person("Dave");
+ person.sayHello();
+
+ person = new Person("Eric");
+ person.sayHello();
+
+/* 
+내가 겪은 예시
+*/
+
+abc = new ABC();
+abc.setA("안녕");
+abc.setB("아침");
+request.setAttribute("아침인사", jsonArray.)
+//abc = new ABC();
+abc.setA("굿바이");
+abc.setB("저녁");
+request.setAttribute("저녁인사", jsonArray.)
+
+ //기본 생성자를 저렇게 2개 작성하면 메모리가 두번쌓여 가비지 컬렉터가 발생한다.
+ //생성자를 지워도 파라미터가 다시 셋해주긴하는데 만일 쿼리가 다 수행되지 않은 상태에서 파라미터를 다시 넣어도 상관없는걸까 (의문점)
+
+/* 
+참조 : https://hbase.tistory.com/209
+*/
